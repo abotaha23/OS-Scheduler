390a1f2a15f404907c1fab3bb5ddf371ae0b4b64
korecodess <131610983+korecodess@users.noreply.github.com>
2023-04-28 21:15:11 +0200

    Update scheduler.c

--- scheduler.c
+++ scheduler.c
@@ -20,71 +20,208 @@
 /*******************************************************************************
  *                     global variables                                    *
  *******************************************************************************/
-SCHEDULING_ALGORITHM Scheduler; 
-int processNumbers;/*Number of processes*/
-int timeChunk;/*special variable for the case of Round Robin time slite*/
-int msgq_id;/*msg queue to communicate between the process_generator and the scheduler*/
+SCHEDULING_ALGORITHM Scheduler;
+int processNumbers; /*Number of processes*/
+int timeChunk;      /*special variable for the case of Round Robin time slite*/
+int msgq_id;        /*msg queue to communicate between the process_generator and the scheduler*/
+msgbuff recProcess;
+int rec_val;
+int stat_loc;
+PCB processTable[MAXSIZE];
+int index = 1; // current index in the processTable
 /*******************************************************************************
- *                      Functions definitions                                    
-********************************************************************************/
+ *                      Functions definitions
+ ********************************************************************************/
 
-void Scheduler_init(int count,SCHEDULING_ALGORITHM s,int chunk){
-// int x=s;
-// printf("Algorithm =%d\n",x);
-// printf("Hello im the Scheduler Process\nNumber of process1 = %d\n",count);
-    Scheduler=s;
-    processNumbers=count;
-    timeChunk=chunk;
-     key_t key_id;
+void Scheduler_init(int count, SCHEDULING_ALGORITHM s, int chunk)
+{
+    // int x=s;
+    // printf("Algorithm =%d\n",x);
+    // printf("Hello im the Scheduler Process\nNumber of process1 = %d\n",count);
+    Scheduler = s;
+    processNumbers = count;
+    timeChunk = chunk;
+    key_t key_id;
     key_id = ftok("keyfile", 65);
-     msgq_id = msgget(key_id, 0666 | IPC_CREAT);
-      if (msgq_id == -1)
+    msgq_id = msgget(key_id, 0666 | IPC_CREAT);
+    if (msgq_id == -1)
     {
         perror("Error in create");
         exit(-1);
     }
+}
+/*
+//void Scheduler_processStop();
+/**
+ * Descritption:checks if there is a new process arrived from the process
+ * generator and if so it pushes it in your data structure passed
+*/
+void Scheduler_recieveNewProcess(void *container)
+{
 
+    if (Scheduler == HPF || Scheduler == SRTN)
+    {
+        int *processQueue = (int *)container;
+        int last = -1;
+        recProcess.process.processNumber = -1;
+        do
+        {
+            last = recProcess.process.processNumber;
+            rec_val = msgrcv(msgq_id, &recProcess, sizeof(recProcess.process), 0, !IPC_NOWAIT);
+            if (recProcess.process.processNumber == last)
+            {
+                break; // no process arrived
+            }
+            push(recProcess.process);
+            processNumbers--;
+        } while (recProcess.process.processNumber != last);
+    }
+    else
+    {
+        // RR
+        /*To Do : implement the same logic*/
+    }
 }
 
+/**
+ * fork the process
+ * return : the pid of the forked process
+ */
 
+int Scheduler_processStart(process_par *newProcess)
+{
+    int pid = fork();
+    if (pid < 0)
+    {
+        perror("Error  in forking a Process\n");
+        exit(-1);
+    }
+    if (pid == 0)
+    { // child process
+        char *runTime = malloc(sizeof(char));
+        snprintf(runTime, sizeof(runTime), "%d", newProcess->runtime);
+        char *processFile[] = {"./process", runTime, NULL}; // arguments for execv
+        execv(processFile[0], processFile);                 // execute the child process
+        fprintf(stderr, "Exec failed\n");                   // execv only returns if it fails
+        exit(1);
+    }
+    processTable[index].arrival_time = newProcess->arrival_time;
+    processTable[index].excutionTime = newProcess->runtime;
+    processTable[index].startTime = getClk();
+    processTable[index].lastTimeStartted = processTable[index].startTime;
+    processTable[index].lastTimeStopped = 0;
+    processTable[index].pid = pid;
+    processTable[index].priority = newProcess->priority;
+    processTable[index].remainingTime = processTable[index].excutionTime;
+    processTable[index].status = RUNNING;
+    processTable[index].waitingTime = 0;
+    index++;
+    return pid;
+}
 
+void Scheduler_processResume(int pid, int processNumber)
+{
+    kill(pid, SIGCONT);
+    // To Do :Update the PCB
+    processTable[processNumber].waitingTime += processTable[processNumber].lastTimeStopped - getClk();
+    processTable[processNumber].status = RUNNING;
+    processTable[processNumber].lastTimeStartted = getClk();
+}
 
+void Scheduler_processStop(int pid, int processNumber)
+{
+    kill(pid, SIGTSTP);
+    // To Do :Update The PCB
+    processTable[processNumber].lastTimeStopped = getClk();
+    processTable[processNumber].status = WAITING;
+    processTable[processNumber].remainingTime -= processTable[processNumber].lastTimeStartted - getClk();
+}
 
 /*******************************************************************************
- *                      main function                                    
-********************************************************************************/
+ *                      Main Algorithms definition                              *
+ *******************************************************************************/
+
+void Scheduler_HPF()  
+{
+
+    // int curSize = 0;
+    // process_par heap[MAXSIZE];
+    process_par p;
+    // just checking that every thing is okey.
+    do
+    {
+        Scheduler_recieveNewProcess((void *)heap);
+        if (curSize == 0)
+        {
+            continue;
+        }
+        p = top();
+        pop();
+        // call the function that will execute the process here.
+        Scheduler_processStart(&p);
+        wait(&stat_loc);
+        // printf("%d %d %d %d\n",p.processNumber,p.arrival_time,p.runtime,p.priority);
+        // call the function that will execute the process hear.
+
+        // check if any process has come ans put it in the PQ
+        // ofcourse its not the real implementation of the logic but
+        // it's just temporary and will be replaced later.
+
+    } while (processNumbers);
+}
+
+/*******************************************************************************
+ *                      main function
+ ********************************************************************************/
 int main(int argc, char *argv[])
-{   
-    int rec_val;
+{
     initClk();
-    Scheduler_init(atoi(argv[2]),atoi(argv[1]),atoi(argv[3]));
-    // printf("Arguments=%d\nRecieved Chunk =%d\n",argc,timeChunk);
+    Scheduler_init(atoi(argv[2]), atoi(argv[1]), atoi(argv[3]));
+    printf("%d\n", processNumbers);
+      recProcess.process.processNumber=-1;
+    // // printf("Arguments=%d\nRecieved Chunk =%d\n",argc,timeChunk);
         switch(Scheduler){
         case HPF:
         //call the function of the HPF Algorithm
+        Scheduler_HPF();
         break;
         case SRTN:
         //call the function of SRTN Algorithm
         break;
         case RR:
-        //call the function of the RR Algorithm 
+        //call the function of the RR Algorithm
         break;
-    }   
-    msgbuff recProcess;
-    recProcess.process.process_id=-1;
-    int last =-1;
-    // int lastID=-1;
+    }
+    // process_par p;
+    // p.arrival_time = 1;
+    // p.priority = 1;
+    // p.processNumber = 1;
+    // p.runtime = 6;
+    // int x = Scheduler_processStart(&p);
+    // sleep(2);
+    // Scheduler_processStop(x, p.processNumber);
+    // sleep(2);
+    // // waitpid(x,&stat_loc,0);
+    // // int last =-1;
+    // // int lastID=-1;
+    
+    // Scheduler_processResume(x, p.processNumber);
+    // waitpid(x, &stat_loc, 0);
+
     // int nowID=-2;
-// in main program we cannot have the number of processes in scheduler so relace it 
-//replace it with while process id !=-1
-    while(processNumbers){  
-    rec_val = msgrcv(msgq_id, &recProcess, sizeof(recProcess.process), 7, !IPC_NOWAIT);
-    if(recProcess.process.process_id!=last){
-    printf("nowTime is %d\n",getClk());
-    printf("Process id =%d\nProcess arrivalTime=%d\nProcess runtime=%d\nProcess priority=%d\n",recProcess.process.process_id,recProcess.process.arrival_time,recProcess.process.runtime,recProcess.process.priority);
-    processNumbers--;
-    last =recProcess.process.process_id;
-    }
-    }
+    // in main program we cannot have the number of processes in scheduler so relace it
+    // replace it with while process id !=-1
+    // while(processNumbers){
+
+    // rec_val = msgrcv(msgq_id, &recProcess, sizeof(recProcess.process), 0, !IPC_NOWAIT);
+    //   if(last==recProcess.process.processNumber){
+    //         continue;
+    //     }
+    // printf("nowTime is %d\n",getClk());
+    // printf("Process id =%d\nProcess arrivalTime=%d\nProcess runtime=%d\nProcess priority=%d\n",recProcess.process.processNumber,recProcess.process.arrival_time,recProcess.process.runtime,recProcess.process.priority);
+    // processNumbers--;
+    // last =recProcess.process.processNumber;
+    // }
+
     destroyClk(true);
 }
