119d383ef59bf77e341d1b592d7d7f38fccfa077
korecodess <131610983+korecodess@users.noreply.github.com>
2023-04-29 05:43:11 +0200

    Update scheduler.c
    updated the recProcess function

--- scheduler.c
+++ scheduler.c
@@ -28,7 +28,13 @@ msgbuff recProcess;
 int rec_val;
 int stat_loc;
 PCB processTable[MAXSIZE];
-int index = 1; // current index in the processTable
+int index = 1;         // current index in the processTable
+short lastProcessFlag; // a flag that indicate the last process that is to be recieved
+// it is updated in the scheduler_recieveNewProcess
+// when the scheduler recieves a process with id = -1 just an indicator
+
+queue g_eventQueue;
+
 /*******************************************************************************
  *                      Functions definitions
  ********************************************************************************/
@@ -49,6 +55,7 @@ void Scheduler_init(int count, SCHEDULING_ALGORITHM s, int chunk)
         perror("Error in create");
         exit(-1);
     }
+    Queue_init(&g_eventQueue);
 }
 /*
 //void Scheduler_processStop();
@@ -93,19 +100,16 @@ int Scheduler_processStart(process_par *newProcess)
     return pid;
 }
 
-
-
-
 void Scheduler_recieveNewProcess(void *container)
 {
 
     if (Scheduler == HPF || Scheduler == SRTN)
-    {   
+    {
         // printf("Hello from the function\n");
         int *processQueue = (int *)container;
         int last = -1;
         recProcess.process.processNumber = -1;
-        while(1)
+        while (1)
         {
             last = recProcess.process.processNumber;
             rec_val = msgrcv(msgq_id, &recProcess, sizeof(recProcess.process), 0, IPC_NOWAIT);
@@ -115,23 +119,38 @@ void Scheduler_recieveNewProcess(void *container)
             }
             push(recProcess.process);
             Scheduler_processStart(&recProcess.process);
-            kill(processTable[index-1].pid,SIGTSTP);
+            kill(processTable[index - 1].pid, SIGTSTP);
             // printf("%d\n",curSize);
             processNumbers--;
-        } 
+        }
     }
     else
     {
         // RR
         /*To Do : implement the same logic*/
+        queue *processQueue = (queue *)container;
+        int last = -1;
+        recProcess.process.processNumber = -1;
+        while (1)
+        {
+            last = recProcess.process.processNumber;
+            rec_val = msgrcv(msgq_id, &recProcess, sizeof(recProcess.process), 0, IPC_NOWAIT);
+            if (recProcess.process.processNumber == last)
+            {
+                break; // no process arrived
+            }
+            Queue_push(processQueue, (void *)(&recProcess.process));
+            Scheduler_processStart(&recProcess.process);
+            kill(processTable[index - 1].pid, SIGTSTP);
+            // printf("%d\n",curSize);
+            processNumbers--;
+        }
     }
 }
 
-
-
 void Scheduler_processResume(int processNumber)
-{   
-    int pid=processTable[processNumber].pid;
+{
+    int pid = processTable[processNumber].pid;
     kill(pid, SIGCONT);
     // To Do :Update the PCB
     processTable[processNumber].waitingTime += processTable[processNumber].lastTimeStopped - getClk();
@@ -139,9 +158,9 @@ void Scheduler_processResume(int processNumber)
     processTable[processNumber].lastTimeStartted = getClk();
 }
 
-void Scheduler_processStop( int processNumber)
-{   
-    int pid=processTable[processNumber].pid;
+void Scheduler_processStop(int processNumber)
+{
+    int pid = processTable[processNumber].pid;
     kill(pid, SIGTSTP);
     // To Do :Update The PCB
     processTable[processNumber].lastTimeStopped = getClk();
@@ -149,20 +168,55 @@ void Scheduler_processStop( int processNumber)
     processTable[processNumber].remainingTime -= processTable[processNumber].lastTimeStartted - getClk();
 }
 
+// void Scheduler_generateOutputFiles()
+// {
+//     FILE *Scheduler_file;
+//     Scheduler_file = fopen("Scheduler.log", "w");
+
+//     while (!isEmpty(&g_eventQueue))
+//     {
+//         Event e = *((Event *)Queue_pop(&g_eventQueue));
+//         int processNumber = e.processNumber;
+//         // TO DO: handle the total, remain and wait time after having the data needed
+//         switch (e.type)
+//         {
+//         case PROCESS_STARTED:
+//             fprintf(Scheduler_file, "At time %d process %d started total %d remain %d wait %d\n", e.time, processNumber, processTable[processNumber].excutionTime, e.remainingTime, e.waitingTime);
+//             break;
+
+//         case PROCESS_STOPPED:
+//             fprintf(Scheduler_file, "At time %d process %d stopped total %d remain %d wait %d\n", e.time, processNumber, processTable[processNumber].excutionTime, e.remainingTime, e.waitingTime);
+//         case PROCESS_RESUMED:
+//             fprintf(Scheduler_file, "At time %d process %d resumed total %d remain %d wait %d\n", e.time, processNumber, processTable[processNumber].excutionTime, e.remainingTime, e.waitingTime);
+//             break;
+//         case PROCESS_FINISHED:
+//         int TA=processTable[processNumber].startTime-processTable[processNumber].arrival_time;
+//         float WTA=((float)TA)/processTable[processNumber].excutionTime;
+//         float near=
+//             fprintf(Scheduler_file, "At time %d process %d finished total %d remain 0 wait %d TA %d WTA %d\n", e.time, processNumber, processTable[processNumber].excutionTime, e.waitingTime,);
+//             break;
+//         }
+//     }
+
+//     fclose(fp);
+//     // To DO: Handle scheduler.pref file
+//     //  Scheduler_file = fopen("Scheduler.perf", "w");
+// }
+
 /*******************************************************************************
  *                      Main Algorithms definition                              *
  *******************************************************************************/
 
-void Scheduler_HPF()  
+void Scheduler_HPF()
 {
 
     // int curSize = 0;
     // process_par heap[MAXSIZE];
     process_par p;
-    int j=processNumbers;
+    int j = processNumbers;
     // just checking that every thing is okey.
     do
-    {   
+    {
         // printf("Hello HPF\n");
         Scheduler_recieveNewProcess((void *)heap);
         if (curSize == 0)
@@ -171,18 +225,12 @@ void Scheduler_HPF()
         }
         p = top();
         pop();
-        int i=p.processNumber;
+        int i = p.processNumber;
         // call the function that will execute the process here.
-        printf("Process Number %d will run at time %d\n",i,getClk());// these prints are just for the sense of testing 
+        printf("Process Number %d will run at time %d\n", i, getClk()); // these prints are just for the sense of testing
         Scheduler_processResume(i);
         wait(&stat_loc);
-         printf("Process Number %d finished at time %d\n",i,getClk());
-        // printf("%d %d %d %d\n",p.processNumber,p.arrival_time,p.runtime,p.priority);
-        // call the function that will execute the process hear.
-        
-        // check if any process has come ans put it in the PQ
-        // ofcourse its not the real implementation of the logic but
-        // it's just temporary and will be replaced later.
+        printf("Process Number %d finished at time %d\n", i, getClk());
         j--;
     } while (j);
 }
@@ -195,21 +243,38 @@ int main(int argc, char *argv[])
     initClk();
     Scheduler_init(atoi(argv[2]), atoi(argv[1]), atoi(argv[3]));
     printf("%d\n", processNumbers);
-      recProcess.process.processNumber=-1;
+    recProcess.process.processNumber = -1;
     // // printf("Arguments=%d\nRecieved Chunk =%d\n",argc,timeChunk);
-        switch(Scheduler){
-        case HPF:
-        //call the function of the HPF Algorithm
-        
+    switch (Scheduler)
+    {
+    case HPF:
+        // call the function of the HPF Algorithm
+
         Scheduler_HPF();
         break;
-        case SRTN:
-        //call the function of SRTN Algorithm
+    case SRTN:
+        // call the function of SRTN Algorithm
         break;
-        case RR:
-        //call the function of the RR Algorithm
+    case RR:
+        // call the function of the RR Algorithm
         break;
     }
+
+    // testing the RR recieve new process
+    // queue q;
+    // Queue_init(&q);
+    // sleep(4);
+    // Scheduler_recieveNewProcess((void*)(&q));
+    // struct node* ptr;
+    // ptr=q.front;
+    // int cnt=0;
+    // while(ptr!=q.rear){
+    //     cnt++;
+    //     ptr++;
+    // }
+    // printf("Recieved processes aree %d\n",cnt);
+    // Queue_destroy(&q);
+
     // process_par p;
     // p.arrival_time = 1;
     // p.priority = 1;
@@ -222,7 +287,7 @@ int main(int argc, char *argv[])
     // // waitpid(x,&stat_loc,0);
     // // int last =-1;
     // // int lastID=-1;
-    
+
     // Scheduler_processResume(x, p.processNumber);
     // waitpid(x, &stat_loc, 0);
     // sleep(4);
